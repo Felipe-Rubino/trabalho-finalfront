a191d121ab66943f746667498713875fb0d0e983
MarcosHBritto <marcos.britto@aluno.senai.br>
2023-05-05 22:32:07 -0300

    Armazenamento de dados no LocalStorage

---
+++ Fale_conosco/faleConosco.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nome = document.querySelector("#nome").value;
+  const email = document.querySelector("#email").value;
+  const tel = document.querySelector("#tel").value;
+  const assunto = document.querySelector("#assunto").value;
+  const mensagem = document.querySelector("#mensagem").value;
+
+  localStorage.setItem("nome", nome);
+  localStorage.setItem("email", email);
+  localStorage.setItem("tel", tel);
+  localStorage.setItem("assunto", assunto);
+  localStorage.setItem("mensagem", mensagem);
+
+  form.reset();
+});
