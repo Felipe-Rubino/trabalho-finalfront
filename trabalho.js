53ff8e2fa827fcfbf1aa3531c6fc7bda9eb9fa3a
Your Name <feliperubinosan09@gmail.com>
2023-05-05 23:34:57 -0300

    mudanças no site

--- trabalho.js
+++ trabalho.js
@@ -1,5 +1,23 @@
     const btn = document.getElementById("btn")
+    //const bot = document.getElementById("bot")
 
     btn.addEventListener("click" , (e)  => {
         document.body.classList.toggle("dark", e.target.checked)
     })
+
+    // //onmouseover
+    // bot.addEventListener("mouseover" , function() {
+
+    //     this.document.
+    // })
+
+
+    
+    function mouseOut(){
+        document.querySelector("#bot p").style.display="none"
+    }
+
+
+    function mouseOver(){
+        document.querySelector("#bot p").style.display="block"
+    }
