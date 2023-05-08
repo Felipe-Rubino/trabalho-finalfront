5c3a1c0851de39ba0d0a75dc57590d4ef2df58fe
Felipe-Rubino <feliperubinosan09@gmail.com>
2023-05-08 19:00:04 -0300

    Trabalho 100% finalizado front end

--- trabalhofinal/trabalho.js
+++ trabalhofinal/trabalho.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById("btn");
-const form = document.querySelector("form");
+
 
 //const bot = document.getElementById("bot")
 
@@ -15,20 +15,3 @@ function mouseOver() {
   document.querySelector("#bot p").style.display = "block";
 }
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const nome = document.querySelector("#nome").value;
-  const email = document.querySelector("#email").value;
-  const tel = document.querySelector("#tel").value;
-  const assunto = document.querySelector("#assunto").value;
-  const mensagem = document.querySelector("#mensagem").value;
-
-  localStorage.setItem("nome", nome);
-  localStorage.setItem("email", email);
-  localStorage.setItem("tel", tel);
-  localStorage.setItem("assunto", assunto);
-  localStorage.setItem("mensagem", mensagem);
-
-  form.reset();
-});
